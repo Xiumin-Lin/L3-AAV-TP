e42f2061b4f92f144a8d580fbd9850043c1ac18e
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2021-10-30 01:43:52 +0200

    Initial commit

---
+++ src/liste.cpp
@@ -0,0 +1,17 @@
+#include "liste.h"
+
+Cellule *NouvelCel(Point p) {
+	return NULL;
+}
+
+void InsererCellule(int pl, Cellule *cel, Cellule *liste) {
+
+}
+
+void SupprimerCellule(int pl, Cellule *liste) {
+
+}
+
+void Afficher(Cellule *liste) {
+
+}
---
+++ src/liste.h
@@ -0,0 +1,48 @@
+#ifndef TP_ALGOAV_LISTE_H
+#define TP_ALGOAV_LISTE_H
+
+typedef struct {
+	int x;
+	int y;
+} Point;
+
+typedef struct cel {
+	Point pt;
+	struct cel *prev_cel;
+	struct cel *next_cel;
+} Cellule;
+
+typedef struct {
+	Cellule *first;
+	Cellule *last;
+} Liste;
+
+/**
+ * Create a new cell and fill the cell's fields with the point "p".
+ * @param p 	- the point to be stored in the cell.
+ * @return 		a new cell with the point "p" in it.
+ */
+Cellule *NouvelCel(Point p);
+
+/**
+ * Insert the cell "cel" after the place "pl" in the list "list".
+ * @param pl 	- the place before the cell to be inserted.
+ * @param cel 	- the cell to be inserted.
+ * @param liste - the cell list.
+ */
+void InsererCellule(int pl, Cellule *cel, Cellule *liste);
+
+/**
+ * Delete the cell at position "pl" in the "list" list.
+ * @param pl 	- the position of the cell to be deleted.
+ * @param liste - the cell list.
+ */
+void SupprimerCellule(int pl, Cellule *liste);
+
+/**
+ * Display the list of cells.
+ * @param liste - the cell list.
+ */
+void Afficher(Cellule *liste);
+
+#endif //TP_ALGOAV_LISTE_H
